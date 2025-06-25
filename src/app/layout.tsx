00d6a74e3b65cc1c5a2110ea4dc961fe88d803d5
minju00 <bin3994000@naver.com>
2025-06-25 14:50:44 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "@/components/Navbar/navbar.client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
