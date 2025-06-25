ca28d04d339b47f8a98aa02a4dc71745f9cc8bd4
minju00 <bin3994000@naver.com>
2025-06-25 11:26:33 +0900

    Navbar

--- src/app/(ex)/ex_popup/page.tsx
+++ src/app/(ex)/ex_popup/page.tsx
@@ -2,13 +2,15 @@
 "use client";
 import React, { useState } from "react";
 import PopupClient from "@/components/popup/popup.client";
+import Navbar from "@/components/Navbar/navbar.client";
 export default function ExamplePopup() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <div className="container mt-4">
-      {/* 팝업 모달 */}
-      <PopupClient />
+      {/* 팝업 모달
+      <PopupClient /> */}
+      <Navbar />
     </div>
   );
 }
