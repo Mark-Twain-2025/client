d1b17d3861a4322c83594166f302b088bd98be87
곽예경 <kyun9.k@gmail.com>
2025-06-25 19:38:14 +0900

    ranking page

--- src/components/Ranking/DailyRank.tsx
+++ src/components/Ranking/DailyRank.tsx
@@ -4,8 +4,15 @@ import { Card } from "react-bootstrap";
 
 export default function DailyRank() {
   return (
-    <div>
-      <Card style={{ width: "18rem", padding: "2rem", borderRadius: "20px" }}>
+    <div style={{ margin: "2rem" }}>
+      <Card
+        style={{
+          width: "40rem",
+          padding: "2rem",
+          borderRadius: "20px",
+          textAlign: "center",
+        }}
+      >
         <Table>
           <thead>
             <tr>
@@ -16,6 +23,18 @@ export default function DailyRank() {
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <td>1</td>
+              <td>예경</td>
+              <td>1000</td>
+              <td>23%</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>예경</td>
+              <td>1000</td>
+              <td>23%</td>
+            </tr>
             <tr>
               <td>1</td>
               <td>예경</td>
--- src/components/Ranking/WeeklyTop.tsx
+++ src/components/Ranking/WeeklyTop.tsx
@@ -1,18 +1,54 @@
 "use client";
 import React from "react";
-import { Card } from "react-bootstrap";
+
+const users = [
+  { name: "박00", avatarUrl: "profile2nd.png", profit: 800, rank: 2 },
+  { name: "김00", avatarUrl: "profile1st.png", profit: 1000, rank: 1 },
+  { name: "강00", avatarUrl: "profile3rd.png", profit: 500, rank: 3 },
+];
 
 export default function WeeklyTop() {
+  const maxProfit = Math.max(...users.map((user) => user.profit));
+
   return (
-    <div
-      style={{
-        width: "18rem",
-        padding: "2rem",
-        textAlign: "center",
-      }}
-    >
-      WeeklyTop <br />
-      (Contents)
+    <div className="flex justify-center items-end gap-8 h-[300px] p-4 bg-white rounded-md shadow-md">
+      {users.map((user) => {
+        const heightPercent = (user.profit / maxProfit) * 100;
+
+        return (
+          <div
+            key={user.name}
+            className="relative flex flex-col items-center w-24"
+          >
+            {/* 그래프 */}
+            <div className="relative w-full h-[200px] rounded-md flex items-end justify-center">
+              {/* 막대 */}
+              <div
+                className="w-full bg-yellow-400 rounded-md flex flex-col items-center justify-end relative"
+                style={{ height: `${heightPercent}%` }}
+              >
+                {/* 이미지 + 텍스트*/}
+                <div className="absolute -top-16 flex flex-col items-center">
+                  <img
+                    src={user.avatarUrl}
+                    alt={user.name}
+                    className="w-14 h-14 rounded-full border-2 border-white shadow object-contain"
+                  />
+                  <div className="mt-1 text-sm font-semibold bg-black text-white px-2 py-0.5 rounded-full">
+                    {user.name}
+                  </div>
+                  <div className="text-2xl mt-2 text-white font-semibold">
+                    {user.rank}
+                  </div>
+                  <div className="text-sm mb-2 text-white font-semibold">
+                    {user.profit} 코인
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
