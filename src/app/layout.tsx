32bbee73b04cab2d5d0332af6ade5b91aff62959
jiyeon <dpswpfkgy@naver.com>
2025-06-26 13:24:13 +0900

    [Feat] login, navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,13 +28,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+          integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div style={{ display: "flex", minHeight: "100vh" }}>
           <Navbar />
           <AuthProvider>
-          <main style={{ flex: 1, padding: "32px" }}>{children}</main>
+          <main style={{ flex: 1, padding: 0, margin: 0, width: "100%", minHeight: "100vh" }}>{children}</main>
           </AuthProvider>
         </div>
       </body>
