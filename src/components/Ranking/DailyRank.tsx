5cd8678899d2d2d8ac558ea6fb71c23506f28383
곽예경 <kyun9.k@gmail.com>
2025-06-26 14:50:24 +0900

    minor changes

--- src/components/Ranking/DailyRank.tsx
+++ src/components/Ranking/DailyRank.tsx
@@ -4,46 +4,46 @@ import { Card } from "react-bootstrap";
 
 export default function DailyRank() {
   return (
-    <div style={{ margin: "2rem" }}>
-      <Card
+    <div style={{ margin: "2rem", width: "40rem", textAlign: "center" }}>
+      {/* <Card
         style={{
           width: "40rem",
           padding: "2rem",
           borderRadius: "20px",
           textAlign: "center",
         }}
-      >
-        <Table>
-          <thead>
-            <tr>
-              <th>순위</th>
-              <th>이름</th>
-              <th>보유 코인</th>
-              <th>수익률</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>예경</td>
-              <td>1000</td>
-              <td>23%</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>예경</td>
-              <td>1000</td>
-              <td>23%</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>예경</td>
-              <td>1000</td>
-              <td>23%</td>
-            </tr>
-          </tbody>
-        </Table>
-      </Card>
+      > */}
+      <Table>
+        <thead>
+          <tr>
+            <th>순위</th>
+            <th>이름</th>
+            <th>보유 코인</th>
+            <th>수익률</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>예경</td>
+            <td>1000</td>
+            <td>23%</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>예경</td>
+            <td>1000</td>
+            <td>23%</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>예경</td>
+            <td>1000</td>
+            <td>23%</td>
+          </tr>
+        </tbody>
+      </Table>
+      {/* </Card> */}
     </div>
   );
 }
